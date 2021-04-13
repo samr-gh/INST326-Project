ea7f380f9861db38d72c7fc952845b40022cf848
samr-gh <82420465+samr-gh@users.noreply.github.com>
2021-04-12 21:20:30 -0400

    Update automated.py

---
+++ automated.py
@@ -0,0 +1,9 @@
+def scrape(site):
+    """Scrapes a website for the required information needed to checkout.
+    
+    Args:
+        site(Site object): a website object taken from a store's website. 
+        
+    Returns:
+        List of data required for checking out. 
+    """
