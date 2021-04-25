668fe6e326b074c9b3e4bfd49e8de0744b5dd4ef
samr-gh <82420465+samr-gh@users.noreply.github.com>
2021-04-25 18:13:07 -0400

    Footlocker site
    Created a Shoe class and updated the Site class. Also added a function get_product_fl() that gets product info on Footlocker's site.

--- automated.py
+++ automated.py
@@ -3,22 +3,75 @@ import urllib.request
 from bs4 import BeautifulSoup
 import time
 
-#specify the url
-fl = "https://www.footlocker.com/category/mens/shoes.html"
+def get_product_fl(site):
+     """Takes a Footlocker web page as a parameter and returns the product
+          details as a string. 
+     
+     Args: 
+          site (str): the url of the desired Footlocker page. 
+          
+     Returns: 
+          A tuple of lists that contains details of the products on the page.
+     """
+     product_names = []
+     product_gender = []
+     product_colors = []
+     product_prices = []
+     
+     #specify the url
+     fl = "https://www.footlocker.com/category/mens/shoes.html"
 
-#query the site and return the html
-page = urllib.request.urlopen(fl)
-time.sleep(2)
+     #query the site and return the html
+     page = urllib.request.urlopen(fl)
+     time.sleep(2)
 
-#parse the html
-soup = BeautifulSoup(page, "html.parser")
+     #parse the html
+     soup = BeautifulSoup(page, "html.parser")
 
-# for p in soup.find_all('span', class_='ProductName-primary'):
-#     print(p.text)
-   
-# for p in soup.find_all('span', class_='ProductPrice'):
-#     print(p.text)
+     for p in soup.find_all('span', class_='ProductName-primary'):
+          product_names.append(p.text)
+     
+     for p in soup.find_all('span', class_='ProductName-alt'):
+          gender = (re.search(r"(.*)•", p.text)).group(1)
+          product_gender.append(gender)
 
-for p in soup.find_all("a", class_="ProductCard-link ProductCard-content"):
-     print(p.text)
+     for p in soup.find_all('span', class_='ProductName-alt'):
+          colors = (re.search(r"•(.*)", p.text)).group(1)
+          product_colors.append(colors)
+          
+     for p in soup.find_all('span', class_='ProductPrice'):
+          product_prices.append(float(p.text[1:]))
+     
+     return product_names, product_gender, product_colors, product_prices
 
+class Shoe:
+     """An object that represents features of a shoe on a store's site. 
+     
+     Attributes:
+          name (str): the name of the shoe
+          gender (str): the gender of the shoe
+          color (str): the colors of the shoe
+          price (float): the price of the shoe
+     """
+     name = ""
+     gender = ""
+     color = ""
+     price = 0.0
+     
+     def __init__(self, name, gender, color, price):
+          self.name = name
+          self.gender = gender
+          self.color = color
+          self.price = price
+          
+class Site():
+     """An object that represents products of a website. 
+    
+     Attributes:
+          products (list of Shoe objects): A list of Shoe objects where each
+               object corresponds to a shoe on the site. 
+    """
+    products = []
+    
+    def __init__(self):
+        self.products.append(Shoe())
