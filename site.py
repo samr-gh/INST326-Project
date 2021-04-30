08139a4df22ce164dd36120bc20922f5e58be7ae
samr-gh <82420465+samr-gh@users.noreply.github.com>
2021-04-30 19:00:11 -0400

    comments

--- site.py
+++ site.py
@@ -1,8 +1,8 @@
-class Site():
-    """An object that represents features of a website. 
+# class Site():
+#     """An object that represents features of a website. 
     
-    Will have attributes such as name, fields, etc.
-    """
-    def __init__(self, name, fields):
-        self.name = name
-        self.fields = []
+#     Will have attributes such as name, fields, etc.
+#     """
+#     def __init__(self, name, fields):
+#         self.name = name
+#         self.fields = []
