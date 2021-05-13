4e5af94c2d69682c7680c11ff00c593b87788c84
samr-gh <82420465+samr-gh@users.noreply.github.com>
2021-05-13 19:01:57 -0400

    Big changes
    moved scrape() to the Data class changed get_product_fl() and others

--- automated.py
+++ automated.py
@@ -1,82 +1,85 @@
 
-"""This is code that was for a part of the project, but after talking with
-Instructor Cruz we are taking a different approach and using Selenium. 
-We are keeping this code here just in case as well to show the progress
-that we had made before the change. 
 
-Disregard this code. 
-"""
 import urllib.request
 import requests
 from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+# from selenium.webdriver.support import expected_conditions as EC
+# from selenium.webdriver.support.ui import WebDriverWait
+# from selenium.webdriver.common.by import By
 import time
 import re
 
 class Data:
-     def get_product_fl(site):
+     def get_product_fl(links):
           """Takes a Footlocker web page as a parameter and returns the product
                details as a string. 
           
           Args: 
-               site (str): the url of the desired Footlocker page. 
+               links (str): the links of the shoes from the Footlocker page. 
                
           Returns: 
-               A tuple of lists that contains details of the products on the page.
+               A list of lists that contains details of the products on the page.
           """
           product_names = []
           product_gender = []
           product_colors = []
           product_prices = []
           product_description = []
-          product_rating = []
           product_sizes = []
           product_num = []
-          product_brand = []
-          
-          #specify the url
-          # fl = "https://www.footlocker.com/category/womens/shoes.html?currentPage=0"
 
-          #query the site and return the html
-          page = urllib.request.urlopen(site)
-          time.sleep(2)
+          for link in links:
+               #query the site and return the html
+               page = urllib.request.urlopen(link)
+               time.sleep(2)
 
-          #parse the html
-          soup = BeautifulSoup(page, "html.parser")
+               #parse the html
+               soup = BeautifulSoup(page, "html.parser")
 
-          for p in soup.findAll('span', class_='ProductName-primary'):
+               p = soup.find('span', class_='ProductName-primary')
                product_names.append(p.text)
-          
-          for p in soup.findAll('span', class_='ProductName-alt'):
+               
+               p = soup.find('span', class_='ProductName-alt')
                product_gender.append(p.text)
-
-          for p in soup.findAll('div', class_='ProductDetails-form__info'):
-               colors = (re.search(r'(.*)', p.text))
-               product_colors.append(colors)
                
-          for p in soup.findAll('span', class_='ProductPrice'):
-                product_prices.append(float(p.text[1:]))
-          
-          for p in soup.findAll('div', class_='ProductDetails-description'):
+               p = soup.find('p', class_='ProductDetails-form__label')
+               product_colors.append(p.text)
+                    
+               p = soup.find('span', class_='ProductPrice')
+               try:
+                    product_prices.append(float(p.text[1:]))
+               except ValueError:
+                    price = re.search(r"to \$(.*)\$.*\$", p.text).group(1)
+                    product_prices.append(float(price))
+               
+               p = soup.find('div', class_='ProductDetails-description')
                product_description.append(p.text)
-     
-          for p in soup.findAll('a', class_='bv-rating bv-text-link bv-popup-target bv-focusable'):
-               product_rating.append(p.text)
-               
-          for p in soup.findAll('div', class_='ProductSize-group'):
+                    
+               p = soup.find('div', class_='ProductSize-group')
                product_sizes.append(p.text)
-          
-          for p in soup.findAll('div', class_='Tab-panel'):
-               number = (re.search(r"#:(.*) ", p.text)).group(1)
+               
+               p = soup.find('div', class_='Tab-panel')
+               number = re.search(r"#: (\w+)", p.text).group(1)
                product_num.append(number)
-          
-          for p in soup.findAll('span', class_='ProductName-primary'):
-               brand = (re.search(r"/w+/s", p.text))
-               product_brand.append(brand)
-          
-          print(product_names, product_gender, product_colors, product_prices, product_description, product_rating, product_sizes, product_num, product_brand)
+        
+          print({"Name": product_names, "Gender": product_gender, 
+                 "Colors: ": product_colors, "Price": product_prices,
+                 "Description": product_description, "Sizes": product_sizes,
+                 "Product ID": product_num})
           
           
      def get_product_link(site):
+          """Takes a url for a page of products and returns a list of links
+               of each specific product.
+               
+          Args:
+               site (str): the url of the site page for links to be extracted.
+               
+          Returns:
+               A list of links
+          """
           list_links = []
           page = urllib.request.urlopen(site)
           #page = requests.get(site).url
@@ -89,8 +92,56 @@ class Data:
                list_links.append("https://www.footlocker.com" + p.get("href"))
           return list_links
           
+     def scrape(url):
+          """Uses Selenium to iterate through all pages of Footlocker's Men's shoes. 
+          Calls the Data class from automated.py to store the data to a tuple. 
+          
+          Args:
+          url (str): the url of the Footlocker website. 
+               
+          Returns:
+          A list of links to each Men's shoe on the url page.
+          """
+          shoe_links = []
+          state = True
+          
+          shoe_links.extend(Data.get_product_link(url))
+          
+          PATH = "C:\Program Files (x86)\chromedriver.exe"
+          driver = webdriver.Chrome(PATH)
+          driver.get(url)
+          time.sleep(5)
+          webdriver.ActionChains(driver).send_keys(Keys.ESCAPE).perform()
+          time.sleep(3)
+          # try:
+          #     agree = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, '//*[@id="touAgreeBtn"]/div/div')))
+          #     agree.click()
+          # except:
+          #     driver.close()
+          #driver.find_elements_by_link_text("I AGREE").click()
+          
+          i = 1
+          while i < 2:#state == True:
+               try:
+                    i += 1
+                    #print("loop " + str(i - 1))
+
+                    driver.find_elements_by_link_text(str(i))[0].click()
+                    time.sleep(2)
+                    new = driver.current_url
+                    r = requests.get(new).url
+                    shoe_links.extend(Data.get_product_link(r))
+               except IndexError:
+                    #driver.close()
+                    state = False
+
+          return shoe_links
+
 url = "https://www.footlocker.com/category/womens/shoes.html?currentPage=0"
-Data.get_product_link(url)
-Data.get_product_fl("https://www.footlocker.com/product/nike-air-force-1-07-le-low-womens/D8959100.html")
+#Data.get_product_link(url)
+s = Data.scrape(url)
+print(s[0])
+print("len: ", len(s))
+#Data.get_product_fl(s)
 
 #get_product_fl()
--- site.py
+++ site.py
@@ -1,88 +1,10 @@
-"""Talked to Instructor Cruz on April, 30 after discussion and he told us to 
-    just have an outline of all of our functions and classes because we changed 
-    our app functionality and he did not expect us to have functional code 
-    written down. 
-"""
 
-import requests
-import time
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+
 from automated import Data
 
-def scrape(url):
-    """Uses Selenium to iterate through all pages of Footlocker's Men's shoes. 
-        Calls the Data class from automated.py to store the data to a tuple. 
-    
-    Args:
-        url (str): the url of the Footlocker website. 
-        
-    Returns:
-        A list of links to each Men's shoe on the url page.
-    """
-    shoe_links = []
-    state = True
-    
-    shoe_links.extend(Data.get_product_link(url))
-    
-    PATH = "C:\Program Files (x86)\chromedriver.exe"
-    driver = webdriver.Chrome(PATH)
-    driver.get(url)
-    time.sleep(4)
-    webdriver.ActionChains(driver).send_keys(Keys.ESCAPE).perform()
-    
-    i = 1
-    while state == True:
-        try:
-            i += 1
-            print("loop " + str(i - 1))
-
-            driver.find_elements_by_link_text(str(i))[0].click()
-            time.sleep(2)
-            new = driver.current_url
-            r = requests.get(new).url
-            print("R: " + r)
-            shoe_links.extend(Data.get_product_link(r))
-            print(len(shoe_links))
-        except IndexError:
-            driver.close()
-            state = False
-
-    return shoe_links
-
-url = "https://www.footlocker.com/category/womens/shoes.html?currentPage=0"
-scrape(url)
-
-class Shoe:
-     """An object that represents features of a shoe on Footlocker.  
-     
-     Attributes:
-          name (str): the name of the shoe
-          gender (str): the gender of the shoe
-          color (str): the colors of the shoe
-          price (float): the price of the shoe
-          description (str): the description that Footlocker gives the shoe
-          rating (float): the rating of the shoe based on reviews
-          sizes (list): a list of floats representing the shoe sizes
-          product_num (str): the unique product ID of the shoe
-          brand (str): the brand of the shoe
-     """
-     def __init__(self, name, gender, color, price, description, rating, sizes, brand):
-          self.name = name
-          self.gender = gender
-          self.color = color
-          self.price = price
-          self.description = description
-          self.rating = rating
-          self.sizes = sizes
-          self.product_num = product_num
-          self.brand = brand
-              
-     def __repr__(self):
-        pass
 
 def store_features(links):
-    """Gets the attributes of a Shoe object from links of specific shoes
+    """Gets the attributes of a shoe from its specific link
         and stores them in a new csv file.
     
     Args:
@@ -91,7 +13,6 @@ def store_features(links):
     Returns:
         The csv file created. 
     """
-    # for feature in get_product_fl:
     #     links = []
     #     links.append(feature['product_names'])
     #     links.append(feature.product_names)
@@ -106,50 +27,55 @@ def store_features(links):
     # df.to_csv('shoe_attributes.csv', encoding='utf-8')
     pass
 
-def print_features(shoe):
-    """Takes a Shoe object and prints the attributes of it
+url = "https://www.footlocker.com/category/womens/shoes.html?currentPage=0"
+links = Data.scrape(url)
+store_features(links)
+
+def print_features(dict):
+    """Takes a shoe and prints the attributes of it
     
     Args:
-        shoe (Shoe object): the desired shoe
+        dict (dictionary): holds the attributes of the desired shoe
         
     No return
     """
     pass
 
-def find_mean_price(brand):
-    """Finds the mean price of a specific brand of shoe.
+# def find_mean_price(brand):
+#     """Finds the mean price of a specific brand of shoe.
     
-    Args:
-        brand (str): the desired brand 
+#     Args:
+#         brand (str): the desired brand 
         
-    Returns:
-        A float value which is the mean price of all shoes of the brand. 
-    """
-    pass
+#     Returns:
+#         A float value which is the mean price of all shoes of the brand. 
+#     """
+#     pass
 
-def sort_rating(csv):
-    """Sorts the shoes in the csv file from shoes with the highest to
-        lowest rating. 
+# def sort_rating(csv):
+#     """Sorts the shoes in the csv file from shoes with the highest to
+#         lowest rating. 
         
-    Args:
-        csv (csv file): the csv file created by the store_features()
-            function. 
+#     Args:
+#         csv (csv file): the csv file created by the store_features()
+#             function. 
     
-    Returns:
-        A list of the ordered product numbers
-    """
-    pass
+#     Returns:
+#         A list of the ordered product numbers
+#     """
+#     pass
 
-def similar_shoes(shoe):
-    """Finds similar shoes that share at least 3 attributes of the given shoe
+# def similar_shoes(shoe, data):
+#     """Finds similar shoes that share at least 3 attributes of the given shoe
     
-    Args:
-        shoe (Shoe object): the shoe object returned by the main() function. 
+#     Args:
+#         shoe (dictionary): the shoe's features returned by the main() function. 
+#         data (csv): the csv storing the shoe data
         
-    Returns:
-        A list of shoe objects that match
-    """
-    pass
+#     Returns:
+#         A list of shoe objects that match
+#     """
+#     pass
 
 def main(criteria):
     """Takes criteria given by the user and suggests relevant shoes that 
@@ -160,7 +86,7 @@ def main(criteria):
             fields).
             
     Returns:
-        The suggested shoe as a Shoe object
+        The suggested shoe's attributes as a dictionary
     """
     pass
 
