fec5b89fc5593cf8df4a9143b0910577b6c79c05
Craig Eichelberger <craigeich2018@comcast.net>
2021-05-13 20:05:18 -0400

    Merge branch 'main' of https://github.com/samr-gh/INST326-Project into main

---
+++ unit_tests.py
@@ -0,0 +1,12 @@
+""" Documentation: 
+
+    Our project scrapes data off of the FootLocker website in order to use that data to 
+    help shoppers find particular shoes.  We do this by scraping data off of the main site,
+    then scraping individual shoe data and saving those attributes that we have scraped.
+    Once we have saved the individual shoe attributes, we are then able to use those attributes
+    in a function that would simplify the shopping experience for the user online.
+    
+    In order to run our program, the user would need to input certain criteria of a shoe and then run the program.
+    
+    To interpret the output of the program, you would see the shoe that best fits the user criteria and satisfies what they
+    are searching for."""
