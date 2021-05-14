83985b7e8725f9487e979ff78d8f1cecb22c550a
samr-gh <82420465+samr-gh@users.noreply.github.com>
2021-05-13 20:56:03 -0400

    Update automated.py

--- automated.py
+++ automated.py
@@ -14,7 +14,8 @@ class Data:
                details as a string. 
           
           Args: 
-               links (str): the links of the shoes from the Footlocker page. 
+               links (list): a list of links of the shoes from the 
+                    desired Footlocker page. 
                
           Returns: 
                A list of lists that contains details of the products on the page.
@@ -35,36 +36,58 @@ class Data:
                #parse the html
                soup = BeautifulSoup(page, "html.parser")
 
-               p = soup.find('span', class_='ProductName-primary')
-               product_names.append(p.text)
-               
-               p = soup.find('span', class_='ProductName-alt')
-               product_gender.append(p.text)
-               
-               p = soup.find('p', class_='ProductDetails-form__label')
-               product_colors.append(p.text)
-                    
-               p = soup.find('span', class_='ProductPrice')
                try:
-                    product_prices.append(float(p.text[1:]))
-               except ValueError:
-                    price = re.search(r"to \$(.*)\$.*\$", p.text).group(1)
-                    product_prices.append(float(price))
+                    p = soup.find('span', class_='ProductName-primary')
+                    product_names.append(p.text)
+               except AttributeError:
+                    continue
                
-               p = soup.find('div', class_='ProductDetails-description')
-               product_description.append(p.text)
-                    
-               p = soup.find('div', class_='ProductSize-group')
-               product_sizes.append(p.text)
+               try:
+                    p = soup.find('span', class_='ProductName-alt')
+                    product_gender.append(p.text)
+               except AttributeError:
+                    pass
                
-               p = soup.find('div', class_='Tab-panel')
-               number = re.search(r"#: (\w+\d)", p.text).group(1)
-               product_num.append(number)
-        
-          print({"Name": product_names, "Gender": product_gender, 
-                 "Colors: ": product_colors, "Price": product_prices,
-                 "Description": product_description, "Sizes": product_sizes,
-                 "Product ID": product_num})
+               try:
+                    p = soup.find('p', class_='ProductDetails-form__label')
+                    product_colors.append(p.text)
+               except AttributeError:
+                    pass
+               
+               try:    
+                    p = soup.find('span', class_='ProductPrice')
+                    try:
+                         product_prices.append(float(p.text[1:]))
+                    except ValueError:
+                         price = re.search(r"to \$(.*)\$.*\$", p.text).group(1)
+                         product_prices.append(float(price))
+               except AttributeError:
+                    pass
+               
+               try:
+                    p = soup.find('div', class_='ProductDetails-description')
+                    product_description.append(p.text)
+               except AttributeError:
+                    pass
+               
+               try:      
+                    p = soup.find('div', class_='ProductSize-group')
+                    product_sizes.append(p.text)
+               except AttributeError:
+                    pass
+               
+               try:
+                    p = soup.find('div', class_='Tab-panel')
+                    number = re.search(r"#: (\w+\d\w*)", p.text).group(1)
+                    product_num.append(number)
+               except AttributeError:
+                    pass
+               
+          
+          print({"Name": product_names[:3], "Gender": product_gender[:3], 
+                 "Colors: ": product_colors[:3], "Price": product_prices[:3],
+                 "Description": product_description[:3], "Sizes": product_sizes[:3],
+                 "Product ID": product_num[:3]})
           
           
      def get_product_link(site):
@@ -79,7 +102,6 @@ class Data:
           """
           list_links = []
           page = urllib.request.urlopen(site)
-          #page = requests.get(site).url
           time.sleep(2)
           
           #parse the html
@@ -91,13 +113,13 @@ class Data:
           
      def scrape(url):
           """Uses Selenium to iterate through all pages of Footlocker's Men's shoes. 
-          Calls the Data class from automated.py to store the data to a tuple. 
+               Calls the Data class from automated.py to store the data to a tuple. 
           
           Args:
-          url (str): the url of the Footlocker website. 
+               url (str): the url of the Footlocker website. 
                
           Returns:
-          A list of links to each Men's shoe on the url page.
+               A list of links to each Men's shoe on the url page.
           """
           shoe_links = []
           state = True
@@ -107,15 +129,17 @@ class Data:
           PATH = "C:\Program Files (x86)\chromedriver.exe"
           driver = webdriver.Chrome(PATH)
           driver.get(url)
-          time.sleep(5)
-          webdriver.ActionChains(driver).send_keys(Keys.ESCAPE).perform()
-          time.sleep(3)
+          
+          time.sleep(2)
           try:
                driver.find_element_by_id("touAgreeBtn").click()
           except:
                pass
+          time.sleep(3)
+          webdriver.ActionChains(driver).send_keys(Keys.ESCAPE).perform()
+          
           i = 1
-          while i < 2:#state == True:
+          while state == True:
                try:
                     i += 1
                     driver.find_elements_by_link_text(str(i))[0].click()
@@ -130,10 +154,11 @@ class Data:
           return shoe_links
 
 url = "https://www.footlocker.com/category/womens/shoes.html?currentPage=0"
-#Data.get_product_link(url)
-print(Data.scrape(url))
-# print(s[0])
-# print("len: ", len(s))
-#Data.get_product_fl(s)
-
-#get_product_fl()
+# product = "https://www.footlocker.com/product/~/A5AO95KO.html"
+# Data.get_product_fl([product])
+Data.get_product_link(url)
+s = Data.scrape(url)
+print(s[-1])
+print("len: ", len(s))
+Data.get_product_fl(s)
+#Data.get_product_fl(["https://www.footlocker.com/product/birkenstock-arizona-womens/1019046B.html"])
